ded3db74dd2387f34ac5aee292843cae2406d820
Marcin Wydrzyński <wydrzynskimarcin@gmail.com>
2018-08-22 14:39:09 +0200

    show files

--- lua/index.lua
+++ lua/index.lua
@@ -18,8 +18,30 @@ local function write_file(path, content)
     f:close()
 end
 
+function scandir(directory)
+    local i, t, popen = 0, {}, io.popen
+    local pfile = popen('ls -a "'..directory..'"')
+    for filename in pfile:lines() do
+        i = i + 1
+        t[i] = filename
+    end
+    pfile:close()
+    return t
+end
+
 
 ngx.header['content-type'] = 'text/html'
 
 local template = require "template"
-template.render(read_file "/lua/view.html", { message = "Hello, World!" })
+
+local files = {}
+for _, file in ipairs(scandir "/etc/nginx/conf.d") do
+    if file ~= "." and file ~= ".." then
+        table.insert(files, { file= file,content=read_file("/etc/nginx/conf.d/" .. file) })
+    end
+end
+template.render(read_file "/lua/view.html", { files = files })
+
+
+
+
