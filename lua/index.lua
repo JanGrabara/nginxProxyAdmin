5054422f6478672180594a9fb1028501699b2d67
Griffith <jangrabara@gmail.com>
2018-08-23 14:50:31 +0200

    modifiFile

--- lua/index.lua
+++ lua/index.lua
@@ -1,5 +1,7 @@
 local oldreq = require
-local require = function(s) return oldreq('lua.lib.' .. s) end
+local require = function(s)
+    return oldreq("lua.lib." .. s)
+end
 
 local open = io.open
 local function read_file(path)
@@ -20,7 +22,7 @@ end
 
 function scandir(directory)
     local i, t, popen = 0, {}, io.popen
-    local pfile = popen('ls -a "'..directory..'"')
+    local pfile = popen('ls -a "' .. directory .. '"')
     for filename in pfile:lines() do
         i = i + 1
         t[i] = filename
@@ -29,19 +31,28 @@ function scandir(directory)
     return t
 end
 
+local args, err = ngx.req.get_uri_args()
 
-ngx.header['content-type'] = 'text/html'
+if err == "truncated" then
+-- one can choose to ignore or reject the current request here
+end
+
+for key, val in pairs(args) do
+    if type(val) == "table" then
+    else
+        path = "/etc/nginx/conf.d/" .. key
+        write_file(path, val)
+    end
+end
+
+ngx.header["content-type"] = "text/html"
 
 local template = require "template"
 
 local files = {}
 for _, file in ipairs(scandir "/etc/nginx/conf.d") do
     if file ~= "." and file ~= ".." then
-        table.insert(files, { file= file,content=read_file("/etc/nginx/conf.d/" .. file) })
+        table.insert(files, {file = file, content = read_file("/etc/nginx/conf.d/" .. file)})
     end
 end
-template.render(read_file "/lua/view.html", { files = files })
-
-
-
-
+template.render(read_file "/lua/view.html", {files = files})
